92fbbc1ec7704ea2f1e487072c38c5d5502acf25
Enrique Flores <ehf.81@hotmail.com>
2021-05-11 21:31:43 -0700

    edit

---
+++ dblib.js
@@ -0,0 +1,73 @@
+// Add packages
+require("dotenv").config();
+// Add database package and connection string
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+const getTotalRecords = () => {
+  sql = "SELECT COUNT(*) FROM customer";
+  return pool
+    .query(sql)
+    .then((result) => {
+      return {
+        msg: "success",
+        totRecords: result.rows[0].count,
+      };
+    })
+    .catch((err) => {
+      return {
+        msg: `Error: ${err.message}`,
+      };
+    });
+};
+
+const insertBook = (book) => {
+  // Will accept either a book array or book object
+  if (book instanceof Array) {
+    params = book;
+  } else {
+    params = Object.values(book);
+  }
+
+  console.log("params is: ", params);
+
+  const sql = `INSERT INTO book (book_id, title, total_pages, rating, isbn, published_date)
+               VALUES ($1, $2, $3, $4, $5, $6)`;
+
+  return pool
+    .query(sql, params)
+    .then((res) => {
+      return {
+        trans: "success",
+        msg: `Book id ${params[0]} successfully inserted`,
+      };
+    })
+    .catch((err) => {
+      return {
+        trans: "fail",
+        msg: `Error on insert of book id ${params[0]}.  ${err.message}`,
+      };
+    });
+};
+
+// const sumNumbers = (sum) ={
+//   var total = 0;
+//   for (var i =0; i < arguments.length; i++){
+//     var num = parseFloat(arguments[i])
+
+//     if(isNaN(num) === false) {
+//       total +=num
+//     }
+//   }
+//   return total
+// }
+
+// Add this at the bottom
+module.exports.insertBook = insertBook;
+module.exports.getTotalRecords = getTotalRecords;
+// module.exports.sumNumbers = sumNumbers;
--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 // Add required packages
 const express = require("express");
 const app = express();
+const dblib = require("./dblib.js");
 
-require("dotenv").config();
+const multer = require("multer");
+const upload = multer();
+// require("dotenv").config();
+
+// Add middleware to parse default urlencoded form
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Set up EJS
 app.set("view engine", "ejs");
 
-// Add database package and connection string (can remove ssl)
-const { Pool } = require("pg");
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+// Enable CORS (see https://enable-cors.org/server_expressjs.html)
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
+
+// Application folders
+app.use(express.static("public"));
+
+// Add database package and connection string (can remove ssl)
+// const { Pool } = require("pg");
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
 
 // Start listener
 app.listen(process.env.PORT || 3000, () => {
@@ -27,24 +47,91 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/data", (req, res) => {
-  //res.send ("Hello world...");
-  const sql = "SELECT * FROM PRODUCT ORDER BY PROD_ID";
-  pool.query(sql, [], (err, result) => {
-    var message = "";
-    var model = {};
-    if (err) {
-      // console.log("err is:", err);
-      message = `Error - ${err.message}`;
-    } else {
-      // console.log("result.rows is: ", result);
-      // console.log("result.rows is: ", result.rows);
-      message = "success";
-      model = result.rows;
-    }
-    res.render("data", {
-        message: message,
-        model : model
+app.get("/sum", async (req, res) => {
+  res.render("sum", {
+    type: "get",
+  });
+});
+
+app.get("/import", async (req, res) => {
+  const totRecs = await dblib.getTotalRecords();
+  res.render("import", {
+    type: "get",
+    totRecs: totRecs.totRecords,
+  });
+});
+
+// POSTS
+
+app.post("/sum", async (req, res) => {
+  dblib
+    .insertCustomer(req.body)
+    .then((result) => {
+      if (result.trans === "success") {
+        res.render("create", {
+          type: "post",
+          prod: req.body,
+          error: "false",
+        });
+      } else {
+        res.render("create", {
+          type: "post",
+          prod: req.body,
+          error: `Unexpected Error: ${result.msg}`,
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
     });
+});
+
+app.post("/import", upload.single("filename"), async (req, res) => {
+  console.log("hi");
+  console.log(req.file, req.body);
+
+  if (!req.file) {
+    //  if (!req.file || Object.keys(req.file).length === 0) {
+    message = `Error: Import file not uploaded ${req.file}`;
+    return res.json({
+      message: message,
+    });
+  }
+  //Read file line by line, inserting records
+  const buffer = req.file.buffer;
+  const lines = buffer.toString().split(/\r?\n/);
+
+  var numFailed = 0;
+  var numInserted = 0;
+  var errorMessage = [];
+
+  // lines.forEach(async (line) => {
+  for (let line of lines) {
+    product = line.split(",");
+    //console.log(product);
+    let result = await dblib.insertBook(product);
+    // if (isbn === "Null" || published_date === "Null") {
+    //   (isbn = null), (published_date = null);
+    // }
+    if (result.trans === "success") {
+      numInserted++;
+    } else {
+      console.log(result);
+      numFailed++;
+      // errorMessage += `${result.msg} \r\n`;
+      errorMessage.push(result.msg);
+    }
+  }
+  // });
+  //index.js establishes the data for the routes
+  //dblib establishes the logic for the database
+  const totRecs = await dblib.getTotalRecords();
+  res.json({
+    type: "post",
+    numFailed: numFailed,
+    numInserted: numInserted,
+    errorMessage: errorMessage,
+    totRecs: totRecs.totRecords,
+    // totRecords: totRecords,
   });
 });
